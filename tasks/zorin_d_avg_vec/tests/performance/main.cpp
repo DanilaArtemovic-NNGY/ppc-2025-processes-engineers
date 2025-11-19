a15ba538a6f5ce61783ce87fb34404fb46d0f7d5
dazorin <danilaooffzorin@gmail.com>
2025-11-19 19:05:29 +0300

    fix(tests): perf [DZ-1]

--- tasks/zorin_d_avg_vec/tests/performance/main.cpp
+++ tasks/zorin_d_avg_vec/tests/performance/main.cpp
@@ -30,7 +30,7 @@ class ZorinDAvgVecPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType
       input_.push_back(x);
     }
 
-    for (int i = 0; i < 25; i++) {
+    for (int i = 0; i < 26; i++) {
       input_.insert(input_.end(), input_.begin(), input_.end());
     }
   }
