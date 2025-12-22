8392ad612aaa6f6fbbb8acb57f6dd91a2fa701a0
dazorin <danilaooffzorin@gmail.com>
2025-12-22 18:48:41 +0300

    fix(style): clangtidy

--- tasks/zorin_d_bellman_ford/tests/functional/main.cpp
+++ tasks/zorin_d_bellman_ford/tests/functional/main.cpp
@@ -11,37 +11,43 @@
 #include "zorin_d_bellman_ford/mpi/include/ops_mpi.hpp"
 #include "zorin_d_bellman_ford/seq/include/ops_seq.hpp"
 
-// NOLINTBEGIN(performance-unnecessary-value-param)
-// clang-tidy сообщает ложное срабатывание:
-// InType передаётся по значению в коде modules/task
-
 namespace zorin_d_bellman_ford {
 
-class ZorinDBellmanFordFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+struct InTypeWrapper {
+  InType value;
+};
+
+using WrappedTestType = std::tuple<int, std::string>;
+
+class ZorinDBellmanFordFuncTests
+    : public ppc::util::BaseRunFuncTests<InTypeWrapper, OutType, WrappedTestType> {
  public:
-  static std::string PrintTestParam(const TestType &test_param) {
+  static std::string PrintTestParam(const WrappedTestType &test_param) {
     return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
   }
 
  protected:
   void SetUp() override {
-    const auto &test_param = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    const int v = std::get<0>(test_param);
+    const auto &test_param =
+        std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
 
-    input_data_ = MakeInput(v, 3, 0);
+    const int v = std::get<0>(test_param);
+    input_data_.value = MakeInput(v, 3, 0);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return !output_data.empty() && output_data.size() == static_cast<std::size_t>(input_data_.graph.vertex_count) &&
+    return !output_data.empty() &&
+           output_data.size() ==
+               static_cast<std::size_t>(input_data_.value.graph.vertex_count) &&
            output_data[0] == 0;
   }
 
-  InType GetTestInputData() final {
+  InTypeWrapper GetTestInputData() final {
     return input_data_;
   }
 
  private:
-  InType input_data_{};
+  InTypeWrapper input_data_{};
 };
 
 namespace {
@@ -50,28 +56,29 @@ TEST_P(ZorinDBellmanFordFuncTests, SeqMpiSameResult) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 3> kTestParam = {
+const std::array<WrappedTestType, 3> kTestParam = {
     std::make_tuple(10, "v10"),
     std::make_tuple(50, "v50"),
     std::make_tuple(100, "v100"),
 };
 
-const auto kTestTasksList = std::tuple_cat(
-    // NOLINTNEXTLINE(performance-unnecessary-value-param)
-    // clang-tidy: ложное срабатывание в modules/task,
-    // InType передаётся по значению внутри шаблона TaskGetter
-    ppc::util::AddFuncTask<ZorinDBellmanFordMPI, InType>(kTestParam, PPC_SETTINGS_zorin_d_bellman_ford),
-
-    // NOLINTNEXTLINE(performance-unnecessary-value-param)
-    ppc::util::AddFuncTask<ZorinDBellmanFordSEQ, InType>(kTestParam, PPC_SETTINGS_zorin_d_bellman_ford));
+const auto kTestTasksList =
+    std::tuple_cat(
+        ppc::util::AddFuncTask<ZorinDBellmanFordMPI, InTypeWrapper>(
+            kTestParam, PPC_SETTINGS_zorin_d_bellman_ford),
+        ppc::util::AddFuncTask<ZorinDBellmanFordSEQ, InTypeWrapper>(
+            kTestParam, PPC_SETTINGS_zorin_d_bellman_ford));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
-const auto kFuncTestName = ZorinDBellmanFordFuncTests::PrintFuncTestName<ZorinDBellmanFordFuncTests>;
+const auto kFuncTestName =
+    ZorinDBellmanFordFuncTests::PrintFuncTestName<ZorinDBellmanFordFuncTests>;
 
-INSTANTIATE_TEST_SUITE_P(BellmanFordTests, ZorinDBellmanFordFuncTests, kGtestValues, kFuncTestName);
+INSTANTIATE_TEST_SUITE_P(BellmanFordTests,
+                         ZorinDBellmanFordFuncTests,
+                         kGtestValues,
+                         kFuncTestName);
 
 }  // namespace
 
 }  // namespace zorin_d_bellman_ford
-// NOLINTEND(performance-unnecessary-value-param)
--- tasks/zorin_d_bellman_ford/tests/performance/main.cpp
+++ tasks/zorin_d_bellman_ford/tests/performance/main.cpp
@@ -7,48 +7,55 @@
 #include "zorin_d_bellman_ford/mpi/include/ops_mpi.hpp"
 #include "zorin_d_bellman_ford/seq/include/ops_seq.hpp"
 
-// NOLINTBEGIN(performance-unnecessary-value-param)
-// clang-tidy сообщает ложное срабатывание:
-// InType передаётся по значению в коде modules/task
-
 namespace zorin_d_bellman_ford {
 
-class ZorinDBellmanFordPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
+struct InTypeWrapper {
+  InType value;
+};
+
+class ZorinDBellmanFordPerfTests
+    : public ppc::util::BaseRunPerfTests<InTypeWrapper, OutType> {
  protected:
   const int vertex_count = 20000;
   const int edges_per_vertex = 64;
 
-  InType input_data{};
+  InTypeWrapper input_data_{};
 
   void SetUp() override {
-    input_data = MakeInput(vertex_count, edges_per_vertex, 0);
+    input_data_.value = MakeInput(vertex_count, edges_per_vertex, 0);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return !output_data.empty() && output_data.size() == static_cast<std::size_t>(input_data.graph.vertex_count) &&
+    return !output_data.empty() &&
+           output_data.size() ==
+               static_cast<std::size_t>(input_data_.value.graph.vertex_count) &&
            output_data[0] == 0;
   }
 
-  InType GetTestInputData() final {
-    return input_data;
+  InTypeWrapper GetTestInputData() final {
+    return input_data_;
   }
 };
 
 TEST_P(ZorinDBellmanFordPerfTests, RunPerfModes) {
-  const auto &param = GetParam();
-  ExecuteTest(param);
+  ExecuteTest(GetParam());
 }
 
-// NOLINTNEXTLINE(performance-unnecessary-value-param)
-// clang-tidy: ложное срабатывание в modules/task,
-// параметр InType передаётся по значению, изменить сигнатуру невозможно
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, ZorinDBellmanFordMPI, ZorinDBellmanFordSEQ>(PPC_SETTINGS_zorin_d_bellman_ford);
+    ppc::util::MakeAllPerfTasks<InTypeWrapper,
+                                ZorinDBellmanFordMPI,
+                                ZorinDBellmanFordSEQ>(
+        PPC_SETTINGS_zorin_d_bellman_ford);
 
-const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
-const auto kPerfTestName = ZorinDBellmanFordPerfTests::CustomPerfTestName;
+const auto kGtestValues =
+    ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ZorinDBellmanFordPerfTests, kGtestValues, kPerfTestName);
+const auto kPerfTestName =
+    ZorinDBellmanFordPerfTests::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests,
+                         ZorinDBellmanFordPerfTests,
+                         kGtestValues,
+                         kPerfTestName);
 
 }  // namespace zorin_d_bellman_ford
-// NOLINTEND(performance-unnecessary-value-param)
