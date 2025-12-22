d35cff47da22c00813ee11462c53c673c3f6c3f8
dazorin <danilaooffzorin@gmail.com>
2025-12-22 19:39:28 +0300

    fix(style): clangtidy

--- tasks/zorin_d_bellman_ford/tests/functional/main.cpp
+++ tasks/zorin_d_bellman_ford/tests/functional/main.cpp
@@ -6,44 +6,37 @@
 #include <tuple>
 
 #include "util/include/func_test_util.hpp"
-#include "util/include/util.hpp"
 #include "zorin_d_bellman_ford/common/include/common.hpp"
 #include "zorin_d_bellman_ford/mpi/include/ops_mpi.hpp"
 #include "zorin_d_bellman_ford/seq/include/ops_seq.hpp"
 
 namespace zorin_d_bellman_ford {
 
-struct InTypeWrapper {
-  InType value;
-};
-
-using WrappedTestType = std::tuple<int, std::string>;
-
-class ZorinDBellmanFordFuncTests : public ppc::util::BaseRunFuncTests<InTypeWrapper, OutType, WrappedTestType> {
+class ZorinDBellmanFordFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
-  static std::string PrintTestParam(const WrappedTestType &test_param) {
+  static std::string PrintTestParam(const TestType &test_param) {
     return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
   }
 
  protected:
   void SetUp() override {
     const auto &test_param = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-
     const int v = std::get<0>(test_param);
-    input_data_.value = MakeInput(v, 3, 0);
+
+    input_data_ = MakeInput(v, 3, 0);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return !output_data.empty() &&
-           output_data.size() == static_cast<std::size_t>(input_data_.value.graph.vertex_count) && output_data[0] == 0;
+    return !output_data.empty() && output_data.size() == static_cast<std::size_t>(input_data_.graph.vertex_count) &&
+           output_data[0] == 0;
   }
 
-  InTypeWrapper GetTestInputData() final {
+  InType GetTestInputData() final {
     return input_data_;
   }
 
  private:
-  InTypeWrapper input_data_{};
+  InType input_data_{};
 };
 
 namespace {
@@ -52,15 +45,15 @@ TEST_P(ZorinDBellmanFordFuncTests, SeqMpiSameResult) {
   ExecuteTest(GetParam());
 }
 
-const std::array<WrappedTestType, 3> kTestParam = {
+const std::array<TestType, 3> kTestParam = {
     std::make_tuple(10, "v10"),
     std::make_tuple(50, "v50"),
     std::make_tuple(100, "v100"),
 };
 
-const auto kTestTasksList = std::tuple_cat(
-    ppc::util::AddFuncTask<ZorinDBellmanFordMPI, InTypeWrapper>(kTestParam, PPC_SETTINGS_zorin_d_bellman_ford),
-    ppc::util::AddFuncTask<ZorinDBellmanFordSEQ, InTypeWrapper>(kTestParam, PPC_SETTINGS_zorin_d_bellman_ford));
+const auto kTestTasksList =
+    std::tuple_cat(ppc::util::AddFuncTask<ZorinDBellmanFordMPI, InType>(kTestParam, PPC_SETTINGS_zorin_d_bellman_ford),
+                   ppc::util::AddFuncTask<ZorinDBellmanFordSEQ, InType>(kTestParam, PPC_SETTINGS_zorin_d_bellman_ford));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
--- tasks/zorin_d_bellman_ford/tests/performance/main.cpp
+++ tasks/zorin_d_bellman_ford/tests/performance/main.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
 #include <cstddef>
+#include <string>
+#include <string_view>
 
 #include "util/include/perf_test_util.hpp"
 #include "zorin_d_bellman_ford/common/include/common.hpp"
@@ -9,27 +11,23 @@
 
 namespace zorin_d_bellman_ford {
 
-struct InTypeWrapper {
-  InType value;
-};
-
-class ZorinDBellmanFordPerfTests : public ppc::util::BaseRunPerfTests<InTypeWrapper, OutType> {
+class ZorinDBellmanFordPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
-  const int vertex_count = 20000;
-  const int edges_per_vertex = 64;
+  const int kV_ = 8000;
+  const int kEdgesPerVertex_ = 8;
 
-  InTypeWrapper input_data_{};
+  InType input_data_{};
 
   void SetUp() override {
-    input_data_.value = MakeInput(vertex_count, edges_per_vertex, 0);
+    input_data_ = MakeInput(kV_, kEdgesPerVertex_, 0);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return !output_data.empty() &&
-           output_data.size() == static_cast<std::size_t>(input_data_.value.graph.vertex_count) && output_data[0] == 0;
+    return !output_data.empty() && output_data.size() == static_cast<std::size_t>(input_data_.g.vertex_count) &&
+           output_data[0] == 0;
   }
 
-  InTypeWrapper GetTestInputData() final {
+  InType GetTestInputData() final {
     return input_data_;
   }
 };
@@ -38,11 +36,10 @@ TEST_P(ZorinDBellmanFordPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
-const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InTypeWrapper, ZorinDBellmanFordMPI, ZorinDBellmanFordSEQ>(
-    PPC_SETTINGS_zorin_d_bellman_ford);
+const auto kAllPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, ZorinDBellmanFordMPI, ZorinDBellmanFordSEQ>(PPC_SETTINGS_zorin_d_bellman_ford);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
-
 const auto kPerfTestName = ZorinDBellmanFordPerfTests::CustomPerfTestName;
 
 INSTANTIATE_TEST_SUITE_P(RunModeTests, ZorinDBellmanFordPerfTests, kGtestValues, kPerfTestName);
